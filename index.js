3f44a2228cbef3c9a3a4685775bae92d1fae554a
Subin Kim <https://github.com/subin-kim1123>
2021-04-11 23:33:06 -0400

    Completed assignment
    dd
    d
    d
    d
    cd
    cd
    kkk=

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b){
+    return a+b;
+};
+
+function subtract(a, b){
+    return a-b;
+};
+
+function multiply(a, b){
+    return a*b;
+};
+
+function divide(a, b){
+    return a/b;
+};
+
+function increment(a) {
+    return a+1;
+}
+
+function decrement(a) {
+    return a-1;
+}
+
+function makeInt(a) {
+    return a.toString;
+}
+
+function makeInt(a='0x2328') {
+    return a;
+}
+
+function preserveDecimal(a='2.222') {
+    return a;
+}
